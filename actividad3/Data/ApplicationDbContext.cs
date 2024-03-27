8eb3d62981898e5cd38d04095ea9485ce7c1ec4d
gimenavargas <1471366@senati.pe>
2024-03-27 12:41:18 -0500

    conexion a base de datos

--- actividad3/Data/ApplicationDbContext.cs
+++ actividad3/Data/ApplicationDbContext.cs
@@ -1,7 +1,22 @@
-﻿namespace actividad3.Data
-{
-    public class ApplicationDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using actividad3.Models;
 
+namespace actividad3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; } // DbSet para la entidad Empleado
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Aquí puedes configurar restricciones adicionales o relaciones si es necesario
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
